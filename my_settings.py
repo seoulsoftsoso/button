a8eab3ad8baff99dc25e89bfabc604949431dc57
sjyoondd <sj.yoon@seoul-soft.com>
2024-04-19 08:33:01 +0000

    django + mysql

---
+++ my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'button',
+        'USER' : 'dev3',
+        'PASSWORD' : 'qing1105!@', # 설정한 비밀번호로 적어주면 된다.
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+    }
+}
