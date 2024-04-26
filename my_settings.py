b91c31e48071d967e287a4fc24e56ec4e387c684
sjyoondd <sj.yoon@seoul-soft.com>
2024-04-26 10:52:15 +0900

    .

--- my_settings.py
+++ my_settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'NAME': 'button',
         'USER' : 'button_dev3',
         'PASSWORD' : 'qing1105!', # 설정한 비밀번호로 적어주면 된다.
-        'HOST' : 'localhost',
+        'HOST' : '192.168.0.203',
         'PORT' : '3306',
     }
 }
