f6ea0d54d29e5e3e3614a7e0bdaf4e285a4180b1
mes service <mes_service@daum.net>
2024-05-13 16:53:54 +0900

    gitignore 업데이트

--- my_settings.py
+++ my_settings.py
@@ -2,9 +2,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'button',
-        'USER' : 'button_dev3',
-        'PASSWORD' : 'qing1105!', # 설정한 비밀번호로 적어주면 된다.
-        'HOST' : '118.44.218.236',
-        'PORT' : '6000',
+        'USER' : 'root',
+        'PASSWORD' : '1105', # 설정한 비밀번호로 적어주면 된다.
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
