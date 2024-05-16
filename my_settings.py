55084f2ab8d29e638f5d9a6cf7fe58f7403271fa
mes service <mes_service@daum.net>
2024-05-17 08:53:26 +0900

    hello

--- my_settings.py
+++ my_settings.py
@@ -2,9 +2,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'button',
-        'USER' : 'root',
-        'PASSWORD' : '1105', # 설정한 비밀번호로 적어주면 된다.
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
+        'USER' : 'button_dev3',
+        'PASSWORD' : 'qing1105!', # 설정한 비밀번호로 적어주면 된다.
+        'HOST' : '118.44.218.236',
+        'PORT' : '6000',
     },
+    'mongodb': {
+        'ENGINE': 'djongo',
+        'NAME': 'djangoConnectTest',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://<username>:<password>@<cluster-address>/<dbname>?retryWrites=true&w=majority'
+        }
+    }
 }
